f958a99545b6a1bcd12252b08636f42e96bde2ce
jaivy Roy <skkarmasi420@gmail.com>
2025-04-28 16:19:21 +0000

    fix: resolved font import and updated header and footer gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,48 @@
-import type {Metadata} from 'next/types/metadata';
-import {Geist, Geist_Mono} from 'next/font/google';
+import type {Metadata} from 'next';
+import {Inter, Fira_Code} from 'next/font/google';
 import './globals.css';
 import {Navbar} from '@/components/Navbar';
 import {Footer} from '@/components/Footer';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+const inter = Inter({subsets:['latin'], variable: '--font-inter'});
+const firaCode = Fira_Code({subsets: ['latin'], variable: '--font-fira-code'})
 
 export const metadata: Metadata = {
   title: 'BCA Study Hub',
   description: 'Your go-to resource for BCA syllabus, notes, and chapter summaries.',
 };
 
+interface Props {
+  children: React.ReactNode;
+}
+
+
+
+
+
+
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) {  
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="bg-gradient-to-r from-blue-100 to-blue-300">
+      <body className={`${inter.variable} ${firaCode.variable} antialiased bg-gradient-to-r from-blue-500 to-purple-500`}>
+        <div className="bg-gradient-to-r from-purple-500 to-pink-500 ">
         <Navbar />
         </div>
         <main className="container mx-auto py-8">
           {children}
         </main>
-        <div className="bg-gradient-to-r from-blue-100 to-blue-300">
+        <div className="bg-gradient-to-r from-purple-500 to-pink-500 ">
         <Footer />
         </div>
       </body>
+
+      
     </html>
   );
 }
